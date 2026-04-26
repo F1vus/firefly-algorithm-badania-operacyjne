e6ebbfbb4d00bdfbc9331e8282f8bf05b586b786
Fiv_ <nzaikar184@gmail.com>
2026-04-26 22:38:09 +0200

    change animation

--- main.m
+++ main.m
@@ -1,16 +1,23 @@
+clear all; close all;
+
+fobj = @(z) z(1)^2 + z(2)^2;
+
+[x_best, f_best] = firefly_algorithm(fobj, 20, 2, 100, 1, 0.2, -5, 5);
+
 function [x_best, f_best, x, fval] = firefly_algorithm(fobj, m, n, MaxGeneration, B0, alpha, lb, ub)
 
-    % x - populacja świetlików: m x n
     x = lb + rand(m, n) .* (ub - lb);
 
-    % wartości funkcji celu
     fval = zeros(m, 1);
     for i = 1:m
         fval(i) = fobj(x(i, :));
     end
 
-    [X,Y] = meshgrid(-5:0.2:5, -5:0.2:5);
+    [X, Y] = meshgrid(-5:0.2:5, -5:0.2:5);
     Z = X.^2 + Y.^2;
+
+    figure(1)
+
     for t = 1:MaxGeneration
         for i = 1:m
             for j = 1:m
@@ -22,38 +29,24 @@ function [x_best, f_best, x, fval] = firefly_algorithm(fobj, m, n, MaxGeneration
 
                     x(i,:) = x(i,:) + beta * (x(j,:) - x(i,:)) + step;
 
-                    % ograniczenia
                     x(i,:) = max(x(i,:), lb);
                     x(i,:) = min(x(i,:), ub);
 
-                    % aktualizacja wartości funkcji
                     fval(i) = fobj(x(i,:));
-
-                    figure(1)
-                    clf
-                    
-                    contour(X,Y,Z,30)
-                    hold on
-                    
-                    scatter(x(:,1), x(:,2), 80, 'filled')
-
-                    
-
                 end
             end
         end
+
+        [f_best, idx] = min(fval);
+        x_best = x(idx, :);
+
+        clf
+        contour(X, Y, Z, 30)
+        hold on
+        scatter(x(:,1), x(:,2), 80, 'filled')
+        plot(x_best(1), x_best(2), 'r*', 'MarkerSize', 15)
+        title(['Iteracja = ', num2str(t)])
+        grid on
+        drawnow
     end
-
-    [f_best, idx] = min(fval);
-    x_best = x(idx, :);
-    plot(x_best(1), x_best(2), 'r*', 'MarkerSize', 15)
-    title(['Iteracja = ', num2str(t)])
-    grid on
-                    
-    drawnow
-    pause(0.1)
 end
-
-fobj = @(z) z(1)^2 + z(2)^2;
-
-[x_best, f_best] = firefly_algorithm(fobj, 20, 2, 100, 1, 0.2, -5, 5);
